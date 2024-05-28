9f1642c8091e4f8163820321af06a645efc8c684
Yaroslav <romashkoyp@gmail.com>
2024-05-28 10:52:15 +0300

    11.9 Simple end-to-end tests

--- playwright.config.js
+++ playwright.config.js
@@ -70,10 +70,13 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start-prod',
+    url: 'http://127.0.0.1:5000',
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:5000/',
+  },
 });
 
