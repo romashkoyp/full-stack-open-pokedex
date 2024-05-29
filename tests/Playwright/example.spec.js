5d077b5f793b129d5fdd5e1b878e978ca3d8a731
Yaroslav <romashkoyp@gmail.com>
2024-05-29 10:16:34 +0300

    ex 9

--- tests/Playwright/example.spec.js
+++ tests/Playwright/example.spec.js
@@ -7,4 +7,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('page with pokemon Ivysaur can be opened', async ({ page }) => {
+    await page.goto('/')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
